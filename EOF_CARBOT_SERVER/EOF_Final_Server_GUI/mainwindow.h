7617d110caaafc595f388c3e2d4a5f33a3818544
DohyeonP <ehgus922@gmail.com>
2024-02-03 15:29:20 +0900

    server GUI 틀 제작

---
+++ EOF_CARBOT_SERVER/EOF_Final_Server_GUI/mainwindow.h
@@ -0,0 +1,59 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QUdpSocket> // ADD for Communication
+#include <QDebug> // ADD for Debug
+#include <QTextStream> // ADD for message handling
+#include <QString> // ADD for String
+#include <QQueue> // ADD for Queue
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots: // call back function public slots --> QT 에서 정의된 기능
+    void video1_receive();
+    void video2_receive();
+    void video3_receive();
+
+    void control1_send(QByteArray data);
+    void control2_send(QByteArray data);
+    void control3_send(QByteArray data);
+
+    QByteArray joyStick1_receive();
+    QByteArray joyStick2_receive();
+    QByteArray joyStick3_receive();
+
+private slots:
+    void on_pushButtonControl1_clicked();
+    void on_pushButtonControl2_clicked();
+    void on_pushButtonControl3_clicked();
+
+private:
+    QUdpSocket *video1_socket;
+    QUdpSocket *video2_socket;
+    QUdpSocket *video3_socket;
+    QUdpSocket *control1_socket;
+    QUdpSocket *control2_socket;
+    QUdpSocket *control3_socket;
+    QUdpSocket *joyStick1_socket;
+    QUdpSocket *joyStick2_socket;
+    QUdpSocket *joyStick3_socket;
+
+    QQueue<QString> video_queue; // QImage 로 바꿀 것! 나중에
+
+    QQueue<QString> joyStick_queue;
+
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
